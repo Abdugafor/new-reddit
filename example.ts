244812661a42bca89370ed9abaa390f70e35ecce
Abdugafor Laptop <abdugaforgafarov0111@gmail.com>
2023-04-24 17:14:48 +0500

    typescript prctice

---
+++ example.ts
@@ -0,0 +1,10 @@
+interface IHomeTask {
+    data: string;
+    numbericData: number;
+    date: Date;
+    externalData: {
+        basis: number;
+        value: string;
+    }
+}
+    
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
+const GLOBAL_CSS_REGEXP = /\.global\.css/
 
 
 function setupDevtool() {
@@ -21,10 +22,31 @@ module.exports = {
         filename: 'index.js'
     },
     module: {
-        rules: [{
+        rules: [
+            {
             test: /\.[tj]sx?$/,
             use: ['ts-loader']
-        }]
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader', {
+                    loader: 'css-loader',
+                    options: {
+                        modules: {
+                            mode: 'local',
+                            localIdentName: '[name]__[local]--[hash:base64:5]'
+                        }
+                    }
+                }
+            ],
+            exclude: GLOBAL_CSS_REGEXP
+        },
+        {
+            test: GLOBAL_CSS_REGEXP,
+            use: ['style-loader', 'css-loader']
+        }
+    ]
     },
     plugins: [
         new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })
